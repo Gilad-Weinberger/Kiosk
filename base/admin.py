f9da4e43694f173560ca6f16ceecc0aaed7c2a9b
Gilad Weinberger <giladwe21@gmail.com>
2024-12-23 19:44:47 +0200

    add admin filters to Order view

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Order, RabbiOrderBurgers, RabbiOrderHotdogs
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'food', 'pay')
-    list_filter = ('food', 'pay')
+    list_display = ('name', 'food', 'pay', 'date')
+    list_filter = ('food', 'pay', 'date')
     search_fields = ('name', 'food', 'pay')
 
 admin.site.register(Order, OrderAdmin)
